f1322f8516229328eb40f2c592ab9abce7d246d9
nicklucarangel <nicolaslucarangel@gmail.com>
2022-10-10 15:20:39 -0300

    teste

--- challenge_1.py
+++ challenge_1.py
@@ -7,5 +7,4 @@ print(df)
 df.to_excel('desafio_minha_planilha_Nicolas_Rangel.xlsx', sheet_name='Teste',
             na_rep='#N/A', header=True, index=False)
 
-df.to_excel('desafio_minha_planilha_Nicolas_Rangel.xlsx', sheet_name='Teste',
-            na_rep='#N/A', header=True, index=False)
+try = testa
